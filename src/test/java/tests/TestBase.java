918b6b0f3f74f405a9faf13e41d45f1a440a5e0b
Max <mvolkov@komus.net>
2025-02-11 15:47:41 +0300

    automationFix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,9 @@ import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -18,7 +21,24 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         RestAssured.baseURI = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "firefox");
+        Configuration.browserSize = System.getProperty("browserSize", "1280x1024");
+        Configuration.browserVersion = System.getProperty("browserVersion", "123.0");
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://"
+                + System.getProperty("login")
+                + ":"
+                + System.getProperty("pass")
+                + "@"
+                + System.getProperty("host")
+                + "/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true ));
     }
+
 
     @AfterAll
     static void shutDown() {
