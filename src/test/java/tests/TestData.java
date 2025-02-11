d8f37fa4ed02967f205a0a40a994c496638b29ee
Max <mvolkov@komus.net>
2025-02-11 14:40:18 +0300

    automationFix

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -4,11 +4,10 @@ import models.BookDataModel;
 import models.CredentialsModel;
 
 public class TestData {
-   private static String login = "test123456",
-                         password = "Test123456@";
+    private static String login = System.getProperty("USERNAME");
+    private static String password = System.getProperty("PASSWORD");
     public static CredentialsModel credentials = new CredentialsModel(login, password);
     private static String book_isbn = "9781449325862";
     private static String book_title = "Git Pocket Guide";
     public static final BookDataModel book = new BookDataModel(book_isbn, book_title);
 }
-
