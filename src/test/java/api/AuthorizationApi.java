94da4bd082aefd74bdbc7327abe1ae59a5733768
Max <mvolkov@komus.net>
2025-02-11 14:51:54 +0300

    automationFix

--- src/test/java/api/AuthorizationApi.java
+++ src/test/java/api/AuthorizationApi.java
@@ -1,6 +1,4 @@
 package api;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.restassured.response.Response;
 import specs.ApiSpec;
 import models.CredentialsModel;
 import models.LoginResponseModel;
@@ -11,22 +9,13 @@ import static io.restassured.http.ContentType.JSON;
 public class AuthorizationApi {
 
     public LoginResponseModel login(CredentialsModel credentials){
-        Response response = given()
+        return given()
                 .body(credentials)
                 .contentType(JSON)
                 .when()
                 .post("/Account/v1/Login")
                 .then()
-                .log().all() // Логируем весь ответ для отладки
-                .statusCode(200) // Проверяем, что статус-код 200
                 .spec(ApiSpec.successResponseSpec)
-                .extract().response();
-
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(response.asString(), LoginResponseModel.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse response: " + e.getMessage(), e);
-        }
+                .extract().as(LoginResponseModel.class);
     }
 }
